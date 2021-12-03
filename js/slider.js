30aee02d4a92fc6bd7721b6ea2bafe5c1e9c3dcd
Arlen <arlen123455@gmail.com>
2021-12-03 15:31:48 +0600

    added mobile version for pages and created new page

--- js/slider.js
+++ js/slider.js
@@ -22,3 +22,26 @@ const swiper2 = new Swiper('.products-swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+const swiper3 = new Swiper('.recommend__slider', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+  speed: 800,
+
+  navigation: {
+    nextEl: '.recommend__button-next',
+    prevEl: '.recommend__button-prev',
+  },
+  breakpoints: {
+    sliderPerView: 2,
+    spaceBetween: 40
+  },
+  1024: {
+    sliderPerView: 3,
+    spaceBetween: 40
+  },
+});  
